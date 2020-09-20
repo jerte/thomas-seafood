96f289ae5460777350b58c979c2ba00da961ce57
Josh Thomas <joshrthomas@protonmail.com>
2020-09-20 17:14:03 -0400

    footer done, unstyled

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,22 @@
 // site footer
 
+import info from '../config/info';
+
 const footerStyle = {
+	'border-top': "solid black 3px",
+	'text-align': "center",
+	'line-height': "50%",
 };
 
 const Footer = () => (
-	<div style={footerStyle}>Footer</div>
+	<div className="align-center" style={footerStyle}>
+		
+		{ /* I'd like to align keys & values around '|', something to do later */ }	
+		<br/>
+		{info.map( (x) => (
+			<p key={x.key}>{x.name} | <a href={x.link}>{x.info}</a></p>
+		))}
+	</div>
 );
 
 export default Footer;
--- config/info.js
+++ config/info.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-const info = {
-	
-};
+const info = [	
+	{
+		key: "address",
+		name: "address",
+		info: "14210 Highway 50, Surf City, NC 28445",
+		link: "https://goo.gl/maps/CD6jHNcpg5jJzaAt9",
+	},
+	{
+		key: "phone number",
+		name: "phone number",
+		info: "9103284361",
+		link: "tel:9103284361",
+	},
+	{
+		key: "facebook",
+		name: "facebook",
+		info: "Check us out on Facebook!",
+		link: "https://www.fb.me/thomastackle",
+	},
+	{
+		key: "google reviews",
+		name: "google reviews",
+		info: "Leave us a review on Google!",
+		link: "https://g.page/r/CSQ_ZJlb7X-DEAg/review",
+	},
+	{
+		key: "yelp",
+		name: "yelp",
+		info: "Leave us a review on Yelp!",
+		link: "https://www.yelp.com/biz/thomas-tackle-and-seafood-holly-ridge",
+	},
+
+];
 
 export default info;
